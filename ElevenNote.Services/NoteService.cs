6a32a8c3410ce5f574751767ea9d848408d4f3a7
milesfw <https://milesfw@bitbucket.org/milesfw/1150-day-1.git>
2016-04-13 20:57:27 -0400

    End of day 2

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,40 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<NoteListItemViewModel> GetNotes()
+        {
+            using (var context = new ElevenNoteDbContext())
+            {
+                return
+                    context
+                            .Notes
+                            .Where(e => e.OwnerId == _userId)
+                            .Select(
+                                e =>
+                                    new NoteListItemViewModel
+                                    {
+                                        NoteId = e.NoteId,
+                                        Title = e.Title,
+                                        CreatedUtc = e.CreatedUtc
+                                    })
+                            .ToArray();
+            }
+        }
+    }
+}
